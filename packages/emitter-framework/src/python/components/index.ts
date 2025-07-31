5422fe8fa4c2333848692b1282b9ab1a9c4e8e9e
Mauricio Gardini <mgardini@pinterest.com>
2025-07-31 17:49:36 +0000

    Add Atom

--- packages/emitter-framework/src/python/components/index.ts
+++ packages/emitter-framework/src/python/components/index.ts
@@ -1,5 +1,1 @@
-// Python components will be exported here as they are created
-// Example future exports:
-// export * from "./class-declaration.js";
-// Empty export to make this a valid module until components are added
-export {};
+export * from "./atom/atom.jsx";
