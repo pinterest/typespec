8b51ca415a66403aaf0178cb468c8db7aa85d291
Fiona <fthompson@pinterest.com>
2026-04-14 14:14:44 -0400

    Add Alloy infrastructure, context system, and field components
    Introduce the foundation for the component-based GraphQL emitter:
    
    - Build/config: Add @alloy-js/core, @alloy-js/graphql dependencies,
      configure JSX transpilation (tsconfig, vitest)
    - Context system: GraphQLSchemaContext with ClassifiedTypes,
      ModelVariants, and ScalarVariant interfaces
    - Field components: Field, OperationField, and GraphQLTypeExpression
      for rendering model properties and operations as GraphQL SDL
    - Type resolution: GraphQLTypeExpression handles scalars, models,
      enums, unions, arrays, and nullability using an isInput prop to
      distinguish input vs output context
    - Scalar mappings: Add getGraphQLBuiltinName() for built-in scalar
      identity checks

---
+++ packages/graphql/src/components/fields/field.tsx
@@ -0,0 +1,62 @@
+import { type ModelProperty, getDoc, getDeprecationDetails } from "@typespec/compiler";
+import * as gql from "@alloy-js/graphql";
+import { useTsp } from "@typespec/emitter-framework";
+import { isNullable, hasNullableElements } from "../../lib/nullable.js";
+import { GraphQLTypeExpression } from "./type-expression.js";
+
+export interface FieldProps {
+  /** The model property to render as a field */
+  property: ModelProperty;
+  /** Whether this field is in an input type context */
+  isInput: boolean;
+}
+
+export function Field(props: FieldProps) {
+  const { program } = useTsp();
+
+  const doc = getDoc(program, props.property);
+  const deprecation = getDeprecationDetails(program, props.property);
+
+  return (
+    <GraphQLTypeExpression
+      type={props.property.type}
+      isOptional={props.property.optional}
+      isInput={props.isInput}
+      isNullable={isNullable(program, props.property)}
+      hasNullableElements={hasNullableElements(program, props.property)}
+      targetType={props.property}
+    >
+      {(typeInfo) => {
+        if (props.isInput) {
+          return (
+            <gql.InputField
+              name={props.property.name}
+              type={typeInfo.typeName}
+              nonNull={typeInfo.isList ? typeInfo.itemNonNull : typeInfo.isNonNull}
+              description={doc}
+              deprecated={deprecation ? deprecation.message : undefined}
+            >
+              {typeInfo.isList ? (
+                <gql.InputField.List nonNull={typeInfo.isNonNull} />
+              ) : undefined}
+            </gql.InputField>
+          );
+        }
+
+        return (
+          <gql.Field
+            name={props.property.name}
+            type={typeInfo.typeName}
+            nonNull={typeInfo.isList ? typeInfo.itemNonNull : typeInfo.isNonNull}
+            description={doc}
+            deprecated={deprecation ? deprecation.message : undefined}
+          >
+            {typeInfo.isList ? (
+              <gql.Field.List nonNull={typeInfo.isNonNull} />
+            ) : undefined}
+          </gql.Field>
+        );
+      }}
+    </GraphQLTypeExpression>
+  );
+}
---
+++ packages/graphql/src/components/fields/index.ts
@@ -0,0 +1,7 @@
+export { Field, type FieldProps } from "./field.js";
+export { OperationField, type OperationFieldProps } from "./operation-field.js";
+export {
+  GraphQLTypeExpression,
+  type GraphQLTypeExpressionProps,
+  type GraphQLTypeInfo,
+} from "./type-expression.js";
